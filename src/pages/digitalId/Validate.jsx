13136d2712b196d5f65231fd54aea7efa762f5ec
Daria Golova <danalis969@gmail.com>
2022-05-26 12:35:32 +0300

    delete spaces

--- src/pages/digitalId/Validate.jsx
+++ src/pages/digitalId/Validate.jsx
@@ -10,15 +10,12 @@ function Validate () {
     <div className="flex h-screen overflow-hidden">
       {/* Sidebar */}
       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-
       {/* Content area */}
       <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
         {/*  Site header */}
         <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-
         <main>
           <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
-
             {/* Page header */}
             <div className="mb-8">
               {/* Title */}
@@ -26,7 +23,6 @@ function Validate () {
             </div>
           </div>
         </main>
-
       </div>
     </div>
   )
--- src/pages/digitalId/ValidationLog.jsx
+++ src/pages/digitalId/ValidationLog.jsx
@@ -10,15 +10,12 @@ function ValidationLog () {
     <div className="flex h-screen overflow-hidden">
       {/* Sidebar */}
       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-
       {/* Content area */}
       <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
         {/*  Site header */}
         <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-
         <main>
           <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
-
             {/* Page header */}
             <div className="mb-8">
               {/* Title */}
@@ -26,7 +23,6 @@ function ValidationLog () {
             </div>
           </div>
         </main>
-
       </div>
     </div>
   )
