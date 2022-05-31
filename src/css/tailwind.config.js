5b1ff31e01fe1f8ac859ee824c1f073c85f67ea8
Daria Golova <danalis969@gmail.com>
2022-05-31 16:31:25 +0300

    changed the array, added props

--- src/css/tailwind.config.js
+++ src/css/tailwind.config.js
@@ -23,7 +23,6 @@ module.exports = {
         descriptionSize: ['0.75rem', { lineHeight: '1.25' }],
         xxs: ['0.625rem', { lineHeight: '0.75' }],
         xs: ['0.75rem', { lineHeight: '1.5' }],
-        descriptionSize: ['0.75rem', { lineHeight: '1.25' }],
         sm: ['0.875rem', { lineHeight: '1.5715' }],
         base: ['1rem', { lineHeight: '1.5', letterSpacing: '-0.01em' }],
         lg: ['1.125rem', { lineHeight: '1.5', letterSpacing: '-0.01em' }],
