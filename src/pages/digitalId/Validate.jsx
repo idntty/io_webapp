ec700318ae079b8d384f1515aba12fa89951026b
DwCay <duminalexei@gmail.com>
2022-05-31 13:58:27 +0300

    Conflict fix

--- src/pages/digitalId/Validate.jsx
+++ src/pages/digitalId/Validate.jsx
@@ -65,12 +65,10 @@ function Validate () {
     <div className="flex h-screen overflow-hidden">
       {/* Sidebar */}
       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-
       {/* Content area */}
       <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
         {/*  Site header */}
         <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-
         <main>
           <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
             <div className="w-fit">
@@ -115,7 +113,6 @@ function Validate () {
           </div>
           <ValidatePanel validatePanelOpen={validatePanelOpen} setValidatePanelOpen={setValidatePanelOpen}/>
         </main>
-
       </div>
     </div>
   )
--- src/pages/digitalId/ValidationLog.jsx
+++ src/pages/digitalId/ValidationLog.jsx
@@ -106,15 +106,12 @@ function ValidationLog () {
       <div className="flex h-screen overflow-hidden">
         {/* Sidebar */}
         <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-
         {/* Content area */}
         <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
           {/*  Site header */}
           <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-
           <main>
             <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
-
               {/* Page header */}
               <div className="pb-8 border-b border-zinc-200">
                 {/* Title */}
@@ -183,7 +180,6 @@ function ValidationLog () {
               </div>
             </div>
           </main>
-
         </div>
       </div>
   )
