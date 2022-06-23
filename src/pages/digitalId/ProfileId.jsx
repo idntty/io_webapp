fe1105e9d669b18d48c9b21bbfc59b015f0a853c
Daria Golova <danalis969@gmail.com>
2022-06-23 15:28:29 +0300

    delete useEffect

--- src/pages/digitalId/ProfileId.jsx
+++ src/pages/digitalId/ProfileId.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import Sidebar from '../../partials/Sidebar';
 import Header from '../../partials/Header';
 import ProfileTable from '../../partials/digitalId/ProfileTable';
