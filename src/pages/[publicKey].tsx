d886acf554b21984d90d8909759b8937b06eadeb
Alex Danilov <dai@vt.edu>
2024-10-09 23:41:41 +0300

    Test layout reset fix #5

--- src/pages/[publicKey].tsx
+++ src/pages/[publicKey].tsx
@@ -31,7 +31,6 @@ import AssignForm from '../components/app/forms/AssignForm';
 
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
-import { GridItem } from '../types/grid';
 
 const HOST = 'api.idntty.io';
 
