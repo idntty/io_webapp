33b38db3b1a0f43da227212772e9662834e7b830
Alex Danilov <dai@vt.edu>
2024-12-05 06:24:15 -0600

    LinkedIn widget

--- src/components/app/grid/Widget.tsx
+++ src/components/app/grid/Widget.tsx
@@ -346,7 +346,7 @@ const Widget = React.forwardRef<HTMLDivElement, WidgetProps>(
               href={`https://www.linkedin.com/in/${username}`}
               target="_blank"
               rel="noopener noreferrer"
-              className="font-medium text-white"
+              className="font-bold text-white"
             >
               View LinkedIn profile ↗
             </Link>
