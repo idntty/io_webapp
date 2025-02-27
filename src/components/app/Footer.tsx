ccffc5ad844b7e86ee9afbc117ebf7de7dd706a8
Alex Danilov <dai@vt.edu>
2025-02-27 09:48:01 -0600

    Copyright

--- src/components/app/Footer.tsx
+++ src/components/app/Footer.tsx
@@ -7,7 +7,9 @@ import Link from 'next/link';
 const Footer: React.FC<React.ComponentPropsWithoutRef<'footer'>> = () => {
   return (
     <footer className="flex h-[60px] shrink-0 items-center justify-between self-stretch bg-gray-50 px-[300px] py-[20px]">
-      <div className="text-sm text-gray-500">© Untitled UI 2077</div>
+      <div className="text-sm text-gray-500">
+        Copyright © IDNTTY 2025. All rights reserved.
+      </div>
       <div className="flex items-center gap-[8px]">
         <Mail01 size="16" className="stroke-gray-500" />
         <Link
--- src/components/onboarding/Footer.tsx
+++ src/components/onboarding/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="box-border flex h-[60px] items-center justify-between px-[32px] py-[20px] text-center text-xs text-gray-400">
       <div>
-        {`Copyright © Idntty 2023. All rights reserved. All other trademarks are the property of their respective owners. With any collaboration you accept the general `}
+        {`Copyright © IDNTTY 2025. All rights reserved. All other trademarks are the property of their respective owners. With any collaboration you accept the general `}
         <a
           className="text-inherit"
           href="https://idntty.io"
@@ -11,7 +11,7 @@ export default function Footer() {
         >
           terms
         </a>{' '}
-        and conditions of iconwerk.
+        and conditions of IDNTTY.
       </div>
     </footer>
   );
